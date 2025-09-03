59b49045c97691e355191a4b051aa2e3a656ba93
Austen Lindley <142365695+ThatSmartLoli@users.noreply.github.com>
2025-09-03 09:23:23 -0500

    Readded Assignment 2

---
+++ Assignment2/Source/CSProject/CSProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CSProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CSProject, "CSProject" );
